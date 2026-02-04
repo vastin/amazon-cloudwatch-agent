ba970ceca0a69a3e5d78b4d2552135662c560253
Vastin <3690049+vastin@users.noreply.github.com>
2026-02-04 22:21:27 +0000

    fix test

--- translator/translate/otel/extension/awsproxy/translator_test.go
+++ translator/translate/otel/extension/awsproxy/translator_test.go
@@ -10,9 +10,12 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/confmap"
+
+	"github.com/aws/amazon-cloudwatch-agent/translator/translate/agent"
 )
 
 func TestTranslate(t *testing.T) {
+	agent.Global_Config.Region = "us-east-1"
 	tt := NewTranslator()
 	conf := confmap.NewFromStringMap(map[string]any{"traces": map[string]any{}})
 	got, err := tt.Translate(conf)
